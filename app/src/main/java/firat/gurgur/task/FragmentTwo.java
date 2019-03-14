5715339a758b671d6830813051adee226caa034d
firat <https://blackandroidman@bitbucket.org/blackandroidman/androidexampleone.git>
2019-03-14 16:25:25 +0300

    Favorites bölümü yapıldı Rating bar eklendi.

--- app/src/main/java/firat/gurgur/task/FragmentTwo.java
+++ app/src/main/java/firat/gurgur/task/FragmentTwo.java
@@ -3,11 +3,27 @@ package firat.gurgur.task;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.gson.Gson;
+
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+
 public class FragmentTwo extends Fragment {
+
+    FavListPojo storesListProjo;
+    private static RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private static RecyclerView recyclerView;
+
     public FragmentTwo() {
     }
 
@@ -19,6 +35,43 @@ public class FragmentTwo extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_two, container, false);
+        View view= inflater.inflate(R.layout.fragment_two, container, false);
+
+        recyclerView = (RecyclerView) view.findViewById(R.id.fav_list);
+        recyclerView.setHasFixedSize(true);
+
+        layoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+
+        storesListProjo = new Gson().fromJson(parseJSONData(), FavListPojo.class);
+        adapter = new FavsListAdapter(storesListProjo.getDetailList(), getActivity());
+        recyclerView.setAdapter(adapter);
+
+
+        return view;
+    }
+    public String parseJSONData() {
+        String JSONString = null;
+        JSONObject JSONObject = null;
+        try {
+
+            InputStream inputStream = getResources().getAssets().open("favorites.json");
+
+            int sizeOfJSONFile = inputStream.available();
+
+            byte[] bytes = new byte[sizeOfJSONFile];
+
+            inputStream.read(bytes);
+
+            inputStream.close();
+
+            JSONString = new String(bytes, "UTF-8");
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+        return JSONString;
     }
 }
